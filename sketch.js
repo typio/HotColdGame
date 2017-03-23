9cabc164ab696aafc65e04e7adfba296c4ccd8cb
typio <tomsemail2100@gmail.com>
2017-03-22 19:04:05 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,7 @@
 var gameWon = false;
 
+var pressed = false;
+
 var t = "Where is the laire of the rare square?"
 
 var rectangle = {
@@ -26,11 +28,14 @@ function setup() {
 
   rectangle.x = random(75, windowWidth - rectangle.l/2);
   rectangle.y = random(75, windowHeight - rectangle.l/2 + 4);
+  rectangle.l = windowHeight/6;
 }
 
 function draw() {
   rectMode(CENTER);
   ellipseMode(CENTER);
+
+  restart();
 
   var colRect = ('rgba(0, 0, 0, 0)');
 
@@ -44,6 +49,8 @@ function draw() {
   if (rectangle.x - rectangle.l / 2 < mouseX && rectangle.x + rectangle.l / 2 > mouseX && rectangle.y - rectangle.l / 2 < mouseY && rectangle.y + rectangle.l / 2 > mouseY) {
     colRect = (255);
     gameWon = true;
+  } else {
+    gameWon = false;
   }
 
   noStroke();
@@ -58,6 +65,22 @@ function draw() {
   ellipse(circle.x, circle.y, circle.d, circle.d);
 
   distanceToRect = int(dist(circle.x, circle.y, rectangle.x, rectangle.y));
+
+}
+
+function restart() {
+  if (gameWon && pressed) {
+    rectangle.x = random(75, windowWidth - rectangle.l/2);
+    rectangle.y = random(75, windowHeight - rectangle.l/2 + 4);
+  }
+}
+
+function mousePressed() {
+  pressed = true;
+}
+
+function mouseReleased() {
+  pressed = false;
 }
 
 //to-do
