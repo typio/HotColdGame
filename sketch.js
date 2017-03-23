5d4b3473ecb437bd29554e7bee1a753951c981fe
typio <tomsemail2100@gmail.com>
2017-03-22 17:10:53 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,68 @@
+var gameWon = false;
+
+var t = "Where is the laire of the rare square?"
+
+var rectangle = {
+  x : 0,
+  y : 0,
+  l : 150
+};
+
+var circle = {
+  x : 0,
+  y : 0,
+  d : 30
+};
+
+var distanceToRect;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight - 4);
+
+  textSize(50);
+  textAlign(CENTER);
+  fill(255);
+  text(t, windowWidth/2, 100);
+
+  rectangle.x = random(75, windowWidth - 75/2);
+  rectangle.y = random(75, windowHeight - 79/2);
+}
+
+function draw() {
+  rectMode(CENTER);
+  ellipseMode(CENTER);
+
+  var colRect = ('rgba(0, 0, 0, 0)');
+
+  var colEllipse = (255);
+
+  colEllipse = map(distanceToRect, 0, sqrt(sq(windowWidth) + sq(windowHeight)), 0, 255);
+
+  background(51);
+
+  if (rectangle.x - rectangle.l / 2 < mouseX && rectangle.x + rectangle.l / 2 > mouseX && rectangle.y - rectangle.l / 2 < mouseY && rectangle.y + rectangle.l / 2 > mouseY) {
+    colRect = (255);
+    gameWon = true;
+  }
+
+  noStroke();
+  fill(colRect);
+  rect(rectangle.x, rectangle.y, rectangle.l, rectangle.l);
+
+  circle.x = mouseX;
+  circle.y = mouseY;
+
+  noStroke();
+  fill(colEllipse);
+  ellipse(circle.x, circle.y, circle.d, circle.d);
+
+  distanceToRect = int(dist(circle.x, circle.y, rectangle.x, rectangle.y));
+}
+
+//to-do
+
+/*
+use button to load game
+timer of x seconds to find item
+display title
+*/
